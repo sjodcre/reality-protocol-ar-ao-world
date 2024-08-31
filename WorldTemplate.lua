49dc1fd3d1f21711a54ad56da10d57f4a6d09689
Your Name <you@example.com>
2024-08-31 20:19:09 +0800

    add chat system and static agent

--- WorldTemplate.lua
+++ WorldTemplate.lua
@@ -20,7 +20,8 @@ RealityInfo = {
       Tilemap = {
         Type = 'Fixed',
         Format = 'TMJ',
-        TxId = 'DRDcUfCFETsNnn4oBkubif4k_qeMFxto__DAuUWjf-0', -- TxId of the tilemap in TMJ format
+        TxId = 'koH7Xcao-lLr1aXKX4mrcovf37OWPlHW76rPQEwCMMA', -- TxId of the tilemap in TMJ format
+        -- TxId = 'DRDcUfCFETsNnn4oBkubif4k_qeMFxto__DAuUWjf-0', -- Mine
         -- Since we are already setting the spawn in the middle, we don't need this
         -- Offset = { -10, -10 },
       },
@@ -32,17 +33,29 @@ RealityInfo = {
           TxId = 'k-p6enw-P81m-cwikH3HXFtYB762tnx2aiSSrW137d8',
         }
       },
+      
   }
   
   RealityEntitiesStatic = {
     ['MyNpc'] = {
         Type = 'Avatar',
-        Position = { 10, 10 },
+        Position = { 5, 10 },
         Metadata = {
           DisplayName = 'My NPC',
           SpriteTxId = '0WFjH89wzK8XAA1aLPzBBEUQ1uKpQe9Oz_pj8x1Wxpc',
         },
-      }
+      },
+    ['37I2PFLN_yvDc-sTevh4yg8WYklxD0axT0fWOF_0JOI'] = {
+      Position = { 10, 10 },
+      Type = 'Avatar',
+      Metadata = {
+        DisplayName = 'Fantasy Llama',
+        SkinNumber = 5,
+        Interaction = {
+          Type = 'Default',
+        },
+      },
+    },
   }
   
   --#endregion
